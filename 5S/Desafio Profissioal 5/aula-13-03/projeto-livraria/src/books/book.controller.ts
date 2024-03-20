6e32c13af63eaaefefe485e599d220f30fc1c14b
Gabriel S. Guerra <gabrielsestito@hotmail.com>
2024-03-20 07:45:11 -0300

    replicando código prática 13-03

---
+++ 5S/Desafio Profissioal 5/aula-13-03/projeto-livraria/src/books/book.controller.ts
@@ -0,0 +1,16 @@
+import {Request, Response} from 'express'
+import { BookService } from './book.service'
+
+class BookControler {
+    async create(req: Request, res: Response){
+        const book = await new BookService().create(req.body());
+        return res.json;
+    }
+
+    async findById(req: Request, res: Response){
+        const book = await new BookService().findById(req.params.id);
+        return res.json(book);
+    }
+}
+
+export default new BookControler();
