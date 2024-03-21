2c227744d190385e6028ea45122dbb1e17bf5647
Gabriel S. Guerra <gabrielsestito@hotmail.com>
2024-03-20 22:54:05 -0300

    delete

--- 5S/Desafio Profissioal 5/aula-13-03/projeto-livraria/routes.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-import bookController from './src/books/book.controller';
-
-const routes = Router();
-routes.post('/books', bookController.create)
-routes.get('/books/:id', bookController.findById);
-
-export {routes};
