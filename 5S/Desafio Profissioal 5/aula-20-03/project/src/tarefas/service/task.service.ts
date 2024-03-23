3ad18c6a8e44aab53eadb3470b38c3b48d2a84b3
Gabriel S. Guerra <gabrielsestito@hotmail.com>
2024-03-23 00:35:35 -0300

    Update task.service.ts

--- 5S/Desafio Profissioal 5/aula-20-03/project/src/tarefas/service/task.service.ts
+++ 5S/Desafio Profissioal 5/aula-20-03/project/src/tarefas/service/task.service.ts
@@ -11,29 +11,27 @@ class TaskService{
         return await taskRepository.executeCreateTask(task);
     }
 
+
+    //métodos de validação de dados -> deveriam ficar no middleware?
+
     async checkMissingData(data: any){
-        const schemaFields = await Object.keys(taskRepository.getSchemaData());
-        const dataFields = Object.entries(data);
-        const invalidEntries = dataFields.filter(field => !schemaFields.includes(field[0]));
-        const invalidKeys = invalidEntries.map(entry => entry[0]);
-        return invalidKeys;
+        const schemaKeys = await Object.keys(taskRepository.getSchemaData());
+        const dataEntries = Object.entries(data);
+        const invalidEntries = dataEntries.filter(field => !schemaKeys.includes(field[0]));
+        return invalidEntries.map(entry => entry[0]);
     }
 
     async checkInvalidData(data: any){
-        const schemaFields = await Object.keys(taskRepository.getSchemaData());
+        const schemaKeys = await Object.keys(taskRepository.getSchemaData());
         const dataKeys = Object.keys(data);
-        const missingFields = schemaFields.filter(field => !dataKeys.includes(field));
-        return missingFields;
+        return schemaKeys.filter(field => !dataKeys.includes(field));
     }
 
     async checkDataNotEmpty(data: any){
         const entries = Object.entries(data);
         const emptyValues = entries.filter(entry => entry[1] === "");
-        const invalidKeys = emptyValues.map(entry => entry[0]);
-        return invalidKeys;
+        return emptyValues.map(entry => entry[0]);
     }
-
-
 
 
 }
